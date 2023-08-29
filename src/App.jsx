43549780b04a1553a6310e24d5aec18242509a5c
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-08-29 12:24:16 +0100

    navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  BallCanvas,
+  Navbar,
+  Contact,
+  Hero,
+  Experience,
+  Tech,
+  Works,
+  EarthCanvas,
+  Feedbacks,
+  StarsCanvas,
+  ComputersCanvas,
+} from './components';
+
 const App = () => {
-  return <div>3D MOTION</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
