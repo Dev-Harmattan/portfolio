6469b20559f8d6cf30d202833a85b9ed1e867f86
Sodiq Ganiyu <ganiyusodiq059@gmail.com>
2023-08-30 16:31:25 +0100

    Tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { technologies } from '../constants';
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../HOC';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center items-center">
+      {technologies.map((tech) => (
+        <div key={tech.name} className="h-28 w-28">
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, 'tech');
